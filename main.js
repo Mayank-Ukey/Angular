a6426216e217275e7a7e5049a7d2b1d1e0ebd5eb
Mayank_Ukey <mayank_ukey@persistent.com>
2021-02-08 13:52:49 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var message = "I am Mayank Ukey and I am the best in the world.";
+console.log(message);
+var x = 10; // let declarations can be done without initializations.
+var y = 20; // const declrations are always done with initialization of a value
---
+++ main.ts
@@ -0,0 +1,6 @@
+export {};
+let message = "I am Mayank Ukey and I am the best in the world.";
+console.log(message);
+
+let x = 10; // let declarations can be done without initializations.
+const y = 20; // const declrations are always done with initialization of a value
